8f9b91a5fe06537003ac996f3c1f10e34d0b9b7c
Twigiyama <asitha@tomorrows.company>
2017-03-12 11:38:47 +0000

    Added Couch DB dependancies to project.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "kitura2"
+    name: "kitura2",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1)]
 )
