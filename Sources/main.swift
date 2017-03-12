8c4f3b71578f9932b75eb5d0b2b7426ced8e242a
Twigiyama <asitha@tomorrows.company>
2017-03-12 12:54:34 +0000

    Added basic routes for JSON responses

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,36 @@
-print("Hello, world!")
+import Kitura
+import HeliumLogger
+import LoggerAPI
+import SwiftyJSON
+
+
+HeliumLogger.use()
+
+let router = Router()
+
+
+
+
+router.get("/polls/list") {
+    request, response, next in
+    defer { next() }
+
+    let status = ["status": "ok"]
+    let result = ["result": status]
+    let json = JSON(result)
+
+    response.status(.OK).send(json: json)
+}
+
+router.post("/polls/create") {
+    request, response, next in
+    defer{ next() }
+}
+
+router.post("/polls/vote/:pollid/:option") {
+    requst, response, next in
+    defer{ next() }
+}
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
